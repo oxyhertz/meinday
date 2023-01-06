1c7cf57118a4df812295be661aa9a00cc9bac5a0
Oxyhertz <prolonchuk5@gmail.com>
2023-01-06 23:36:23 +0200

    Ui Libaries connected + new navbar icons

--- src/cmps/board/side-bar.vue
+++ src/cmps/board/side-bar.vue
@@ -1,22 +1,30 @@
 <template>
     <nav class="main-navbar">
-        <div>
+        <div v-tooltip="'Home page'">
             <router-link to="/board" exact>
                 <img src="https://cdn.monday.com/images/logos/monday_logo_icon.png" alt="">
             </router-link>
             <div class="selected-view-indication"></div>
         </div>
         <div class="divider"></div>
-        <div>
+        <div v-tooltip="'Work management'">
             <router-link to="/board/152lj">
                 <board-button />
             </router-link>
             <div class="selected-view-indication"></div>
         </div>
+        <div v-tooltip="'Notifications'">
+            <bell-icon />
+        </div>
+        <div v-tooltip="'Favorites'">
+            <favorite-icon />
+        </div>
     </nav>
 </template>
 <script>
 import boardButton from './board-button.vue';
+import bellIcon from '../icons/bell.vue'
+import favoriteIcon from '../icons/favorite.vue'
 export default {
     name: 'side-bar',
     data() {
@@ -37,7 +45,7 @@ export default {
 
     },
     components: {
-        boardButton
+        boardButton, bellIcon, favoriteIcon
     }
 }
 </script>
