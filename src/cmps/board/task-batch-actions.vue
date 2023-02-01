c4e22bf6c0fb5a69440b650a27ed7af4e6b6c183
Oxyhertz <prolonchuk5@gmail.com>
2023-02-01 10:27:05 +0200

    task-batch-actions basic

---
+++ src/cmps/board/task-batch-actions.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="task-batch-actions">
+
+    </section>
+</template>
+<script>
+export default {
+    name: 'task-batch-actions',
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    components: {
+
+    }
+}
+</script>
