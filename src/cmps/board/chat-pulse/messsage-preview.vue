ffe47c18936207c749279c9287b382917e2e28d4
Oxyhertz <prolonchuk5@gmail.com>
2023-04-15 15:05:30 +0300

    Finito styling of chat msg

--- src/cmps/board/chat-pulse/messsage-preview.vue
+++ src/cmps/board/chat-pulse/messsage-preview.vue
@@ -5,24 +5,37 @@
         <img :src="msg.byMember.imgUrl" alt="user profile pic" />
       </div>
       <p class="user-fullname">{{ msg.byMember.fullname }}</p>
-      <div class="extra-info ">
-        <time-icon/>
-          <p class="relative-time">{{ relatedTime() }}</p>
-         <bell-icon/>
-         <pulse-menu-button />
+      <div class="extra-info">
+        <time-icon />
+        <p class="relative-time">{{ relatedTime() }}</p>
+        <bell-icon />
+        <pulse-menu-button />
       </div>
     </div>
-    <div class="msg-content" v-html="msg.content">
-    </div>
-    <main></main>
-    <footer></footer>
+    <div class="msg-content" v-html="msg.content"></div>
+    <footer>
+        <div>
+            <button>
+                <likeIcon />
+                <span>Like</span>
+            </button>
+        </div>
+        <div>
+            <button>
+                <replyIcon />
+                <span>Reply</span>
+            </button>
+        </div>
+    </footer>
   </section>
 </template>
 <script>
-import bellIcon from '../../icons/bell-icon.vue';
-import timeIcon from '../../icons/time-icon.vue';
-import pulseMenuButton from '../../pulse-menu-button.vue';
-import { utilService } from '../../../services/util.service.js';
+import bellIcon from '../../icons/bell-icon.vue'
+import timeIcon from '../../icons/time-icon.vue'
+import likeIcon from '../../icons/like-icon.vue'
+import replyIcon from '../../icons/reply-icon.vue'
+import pulseMenuButton from '../../pulse-menu-button.vue'
+import { utilService } from '../../../services/util.service.js'
 export default {
     props: {
         msg: {
@@ -37,18 +50,19 @@ export default {
     methods: {
         relatedTime() {
             return utilService.relativeTimeString(this.msg.createdAt)
-        }
+        },
     },
     components: {
         bellIcon,
         timeIcon,
-        pulseMenuButton
+        pulseMenuButton,
+        likeIcon,
+        replyIcon,
     },
 }
 </script>
 <style scoped>
 .msg-container {
-    padding: 10px;
     margin-block-end: 16px;
 }
 
@@ -58,6 +72,7 @@ export default {
     align-items: center;
     gap: 15px;
     margin-block-end: 16px;
+    padding: 16px 16px 8px 16px;
 }
 
 .user-avatar img {
@@ -68,9 +83,6 @@ export default {
     margin-right: 10px;
     object-fit: cover;
 }
-
-
-
 
 .extra-info {
     display: flex;
@@ -91,6 +103,36 @@ export default {
 }
 
 .msg-content {
+    font-size: 16px;
+    padding: 0 16px 8px 16px;
+}
+
+footer {
+    display: flex;
+    border-top: 1px solid #c3c6d4;
+}
+
+footer div {
+    flex-basis: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+footer div:first-child {
+    border-right: 1px solid #c3c6d4;
+
+}
+
+footer button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    background: none;
+    border: none;
+    cursor: pointer;
+    width: 100%;
     font-size: 16px;
 }
 </style>
